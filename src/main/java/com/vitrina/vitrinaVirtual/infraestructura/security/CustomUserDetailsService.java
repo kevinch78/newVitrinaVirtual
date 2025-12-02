5aa49ff48e8d5566a2524c89819dc629476ac2ef
Kevin <chachinoykevin@gmail.com>
2025-12-02 14:22:02 -0500

    cambio

--- src/main/java/com/vitrina/vitrinaVirtual/infraestructura/security/CustomUserDetailsService.java
+++ src/main/java/com/vitrina/vitrinaVirtual/infraestructura/security/CustomUserDetailsService.java
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
-
+//service
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
 
